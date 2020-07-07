b5f1919578d346855034f50d2bde19433348d73a
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-07-07 09:54:49 -0400

    convert indentation to spaces

--- src/main/java/huntertran/corsproxy/CorsProxyApplication.java
+++ src/main/java/huntertran/corsproxy/CorsProxyApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CorsProxyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CorsProxyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CorsProxyApplication.class, args);
+    }
 
 }
