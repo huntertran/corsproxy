7ce36057d7642e8065aa868ee605ff09c0c17d29
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-07-07 09:49:03 -0400

    initial commit

---
+++ src/main/java/huntertran/corsproxy/CorsProxyApplication.java
@@ -0,0 +1,13 @@
+package huntertran.corsproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorsProxyApplication.class, args);
+	}
+
+}
